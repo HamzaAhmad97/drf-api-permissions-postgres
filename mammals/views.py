60d964afe6d3f19c7a7f643314ac20a41877ca2c
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-22 22:31:23 +0200

    build: docker

--- mammals/views.py
+++ mammals/views.py
@@ -1,12 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import BasicAuthentication
 from .serializers import MammalSerializer
 from .models import Mammal
 from .permissions import IsOwnerOrReadOnly,  IsAuthorOrReadOnly
 
 class MammalList(generics.ListCreateAPIView):
-    authentication_classes = [BasicAuthentication]
     permission_classes = (IsAuthenticated,)
     queryset = Mammal.objects.all()
     serializer_class = MammalSerializer
--- zoo/settings.py
+++ zoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#ob)mew2*!m#n-fa_v%l%*@%on1nu*__*+xao7o&!=-p)-u*^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'zoo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
