de41d9b548c083433ad39681e313b8c6fff9f533
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-22 20:44:49 +0200

    feat: updated permissions

--- mammals/views.py
+++ mammals/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import MammalSerializer
 from .models import Mammal
+from .permissions import IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly
 
 class MammalList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Mammal.objects.all()
     serializer_class = MammalSerializer
 
 
 class MammalDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,IsAuthenticatedOrReadOnly)
     queryset = Mammal.objects.all()
     serializer_class = MammalSerializer 
