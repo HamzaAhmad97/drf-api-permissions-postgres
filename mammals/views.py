40724209ad873629dfc985a3520a4956d60b3ce0
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-22 21:29:49 +0200

    feat: added basic authentication

--- mammals/views.py
+++ mammals/views.py
@@ -1,17 +1,18 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 from .serializers import MammalSerializer
 from .models import Mammal
-from .permissions import IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly,  IsAuthorOrReadOnly
 
 class MammalList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = [BasicAuthentication]
+    permission_classes = (IsAuthenticated,)
     queryset = Mammal.objects.all()
     serializer_class = MammalSerializer
 
 
 class MammalDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,IsAuthenticatedOrReadOnly)
+    permission_classes = (IsAuthorOrReadOnly,IsOwnerOrReadOnly,IsAuthenticated)
     queryset = Mammal.objects.all()
     serializer_class = MammalSerializer 
